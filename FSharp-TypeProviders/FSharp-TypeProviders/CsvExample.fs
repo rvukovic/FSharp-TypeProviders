240219ca2c25d07b3c069e33202619f689088019
Robert Vukovic <r.vukovic@levi9.com>
2016-03-18 15:24:10 +0100

    Initial CSV examples

---
+++ FSharp-TypeProviders/FSharp-TypeProviders/CsvExample.fs
@@ -0,0 +1,11 @@
+﻿module CsvExample
+
+open FSharp.Data
+
+type csvProvider = CsvProvider<"MOCK_DATA.csv">
+
+let DoCsv = 
+    let csv = csvProvider.Load("MOCK_DATA.csv")
+
+    for r in csv.Rows do
+        printfn "%A, %s, %F" r.Id r.First_name r.Money 
